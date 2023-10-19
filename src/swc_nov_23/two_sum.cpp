409c8f7d24a9a9e3cb47ff0bacdde316b2dd08a7
Tai <tai2.tran@lge.com>
2023-10-19 17:24:44 +0700

    ud

---
+++ src/swc_nov_23/two_sum.cpp
@@ -0,0 +1,38 @@
+/*
+//input
+2 7 11 15
+9
+// output
+0 1
+
+//input
+3 2 4
+6
+// output
+1 2
+*/
+#include <bits/stdc++.h>
+#include "log.hpp"
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("input.txt","r",stdin);
+
+    vector<int> s_arr;
+    int number;
+    string line;
+    
+
+    cin<<line;
+    stringstream str(line);
+
+    while (str>>number) {
+        s_arr.push_back(number);
+    }
+
+    cout<<s_arr;
+
+    return 0;
+}
