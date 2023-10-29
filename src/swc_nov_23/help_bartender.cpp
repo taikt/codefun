bc4c91efc0707bc8aa6d6a16a45794247f1a0033
taikt <taitrananhvn@gmail.com>
2023-10-30 02:15:28 +0700

    ud

--- src/swc_nov_23/help_bartender.cpp
+++ src/swc_nov_23/help_bartender.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
+#include "log.hpp"
 using namespace std;
 
-const int maxn = 1e9 + 7;
+const int maxn = 1e3 + 7;
 int a[maxn], b[maxn];
 int n, m;
-vector<int> v1[maxn], v2[maxn];
-vector<int> p;
+
 bool v[maxn];
 
-bool bfs(int s, int t) {
+bool bfs(int s, int t, vector<int> v1[], vector<int> v2[]) {
     fill_n(v,n,false);
     vector<int> o1;
     vector<int> o2;
@@ -28,29 +28,34 @@ bool bfs(int s, int t) {
         }
     }
     sort(o1.begin(), o1.end());
+    cout<<o1;
 
     fill_n(v,n,false);
     queue<int> q2;
+    //cout<<"t="<<t<<"\n";
     q2.push(t);
     v[t] = true;
-    while (!q.empty()) {
-        int x = q.front();
+    while (!q2.empty()) {
+        int x = q2.front();
         o2.push_back(x);
-        q.pop();
+        q2.pop();
         for (auto ne : v2[x]) {
             if (!v[ne]) {
                 v[ne] = true;
-                q.push(ne);
+                q2.push(ne);
             }
         }
     }
     sort(o2.begin(), o2.end());
-
+    //cout<<o2;
     if (o1 == o2) return true;
     else return false;
 
 }
 void solve() {
+    vector<int> v1[maxn], v2[maxn];
+    vector<int> p;
+
     cin >> n >> m;
     for (int i = 0; i<n; i++) {
         cin >> a[i];
@@ -73,16 +78,23 @@ void solve() {
     sort(p.begin(), p.end());
 
     for (auto k: p) {
-        if (bfs(a[k], b[k])) cout<<"YES\n";
-        else cout<<"NO\n";
-        
+        if (!bfs(a[k], b[k],v1,v2)) {
+            cout<<"NO\n";
+            return;
+        }
     }
+    cout<<"YES\n";
+
+    fill_n(a,n,0);
+    fill_n(b,n,0);
+   
 }
 
 int main() {
     freopen("input.txt","r",stdin);
 
     int t;
+    cin>> t;
     while(t--) {
          solve();
     }
