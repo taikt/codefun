0b03f1c4d50ae397c59550638d6d04d334373d06
taikt <taitrananhvn@gmail.com>
2024-03-30 15:02:19 +0700

    update

---
+++ src/problems/temp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    freopen("input.txt","r",stdin);
+    
+	int n;
+	cin >> n;
+	for (int q = 0; q < n; q++) {
+		int x;
+		int div_num = 0;
+		cin >> x;
+		for (int i = 1; i * i <= x; i++) {
+			if (x % i == 0) { div_num += i * i == x ? 1 : 2; }
+		}
+		cout << div_num << '\n';
+	}
+}
