e4d5914f252eea26ed127fb62dbe9661308a92f5
Tai <tai2.tran@lge.com>
2024-04-01 17:59:41 +0700

    update

--- src/problems/a.out.cpp
+++ src/problems/a.out.cpp
@@ -1,51 +1,23 @@
 #include <bits/stdc++.h>
-#define INF_INT 2147483647
-#define w(x) cerr << #x << " is " << x << endl;
-#define all(v) v.begin(), v.end()
-typedef long long ll;
 using namespace std;
-
-const int MAX_X = 1e6;
-int MAX_DIV[MAX_X + 1];
-
-void precompute() {
-    for (int i = 2; i <= MAX_X; i++) {
-        if (MAX_DIV[i] != 0) continue;
-        for (int j = i; j <= MAX_X; j += i) {
-            MAX_DIV[j] = i;
-        }
-    }
-}
-
-int count_divisor(int n) {
-    int result = 1;
-    while (n != 1) {
-        int count = 0, prime = MAX_DIV[n];
-		w(n);
-		w(prime);
-        while (n % prime == 0) {	
-            count++;
-            n /= prime;
-        }
-        result *= count + 1;
-    }
-    return result;
-}
-
-void solve() {
-    precompute();
-    int n; cin >> n;
-
-    while (n--) {
-        int x; cin >> x;
-        cout << (count_divisor(x)) << '\n';
-    }
-}
+int n,m;
 
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-	freopen("input.txt","r",stdin);
-    solve();
+	//freopen("input.txt","r",stdin);
+    cin>>n;
+    while (n-->0){
+        cin>>m;
+        int cnt=2;
+        for (int i=2;i*i<=m;i++){
+            if (m%i==0){
+                if (i*i==m)
+                    cnt = cnt+1;
+                else cnt = cnt+2;
+            }
+        }
+        cout<<cnt<<"\n";
+    }
     return 0;
 }
