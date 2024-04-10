1c1eedb875fbf47fdc80151698b5ab05f538ed46
Tai <tai2.tran@lge.com>
2024-04-10 18:13:29 +0700

    update

--- src/problems/solar_charge.cpp
+++ src/problems/solar_charge.cpp
@@ -1,18 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 int T,N,B,C;
-int p[1000],f[1000],d[1000];
-int cost[1000][1000];
+//int p[1000],f[1000],d[1000];
+//int cost[1000][1000];
 
-int solve(int i, int bat) {
-    // dung pin: bat = bat - d[i], if bat > d[i]
-    // dung dien (xac ping): pin = min(pin+p[i],c), cost tang: cost += f[i]*d[i]
-    // dk: tai N, bat >= B, cost min
-    // cost[i,bat]=min(cost[i-1,bat+d[i]], cost[i-1,bat]+f[i]*d[i])
-    // min cost[n,bat], bat >= B
-    if (cost[i][bat] != 1e9) return cost[i][bat];
-    return (cost[i][bat] = min(min(cost[i-1][bat],cost[i-1][min(bat+d[i],C)]), cost[i-1][bat]+f[i]*d[i])); 
-    
+void solve() {
+    // tai day i, dung pin
+    // cost[i][bat-d[i]] = cost[i-1][bat] if bat >= d[i]
+    // tai day i, dung dien
+    // cost[i][min(C,bat+p[i])] = cost[i-1][bat] + f[i]*d[i]
+    // base case:
+    // cost[0][i]=0,B<=i<<C, other=1e9
+    /*
+    for (int i=0;i<=N;i++){
+        for (int j=0;j<=C;j++){
+            if (cost[i][j]==1e9) continue;
+            if (j>=d[i]) cost[i+1][j-d[i]] = min(cost[i+1][j-d[i]],cost[i][j]);
+            cost[i+1][min(C,j+p[i])] = min(cost[i+1][min(C,j+p[i])], cost[i][j] + f[i]*d[i]);
+        }
+    }
+    */
+    vector<int> p(N), f(N),d(N);
+    int ans=1e9;
+    for (int i=0;i<N;i++) cin>>p[i];
+    for (int i=0;i<N;i++) cin>>f[i];
+    for (int i=0;i<N;i++) cin>>d[i];
+    vector<vector<int>> cost(N+1, vector<int>(C+1,1e9));
+    cost[0][B] = 0;
+    /*
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<=C; j++) {
+            if (cost[i][j] == 1e9) continue;
+            int j1 = j - d[i], j2 = min(C, j+p[i]);
+            if (j1 >= 0) cost[i+1][j1] = min(cost[i+1][j1], cost[i][j]);
+            cost[i+1][j2] = min(cost[i+1][j2], cost[i][j] + f[i]*d[i]);
+        }
+    }
+    */
+    for (int i=0;i<=N;i++){
+        for (int j=0;j<=C;j++){
+            if (cost[i][j]==1e9) continue;
+            if (j>=d[i]) cost[i+1][j-d[i]] = min(cost[i+1][j-d[i]],cost[i][j]);
+            cost[i+1][min(C,j+p[i])] = min(cost[i+1][min(C,j+p[i])], cost[i][j] + f[i]*d[i]);
+        }
+    }
+    for (int j=B;j<=C;j++) {
+            ans = min(ans, cost[N][j]);
+    }
+    cout<<ans<<"\n";
+  
 }
 
 int main() {
@@ -22,21 +58,7 @@ int main() {
     cin>>T;
     while (T-- >0){
         cin>>N>>B>>C;
-        fill_n(p,1000,0);
-        fill_n(f,1000,0);
-        fill_n(d,1000,0);
-        int ans=1e9;
-        for (int i=0;i<N;i++) cin>>p[i];
-        for (int i=0;i<N;i++) cin>>f[i];
-        for (int i=0;i<N;i++) cin>>d[i];
-        for (int i=0;i<N;i++){
-            for (int bat=0;bat<=C;bat++)
-            cost[i][bat]=1e9;
-        }
-        //for (int i=0;i<B;i++) cost[0][i]=f[0]*d[0];
-        for (int i=B;i<=C;i++) cost[0][i]=0;
-        for (int j=B;j<=C;j++) ans = min(ans, solve(N-1,j));
-        cout<<ans<<"\n";
+        solve();
     }
 
     return 0;
