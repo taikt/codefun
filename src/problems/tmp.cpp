83dfe69a271a5d600821f77dec83f1295f803740
Tai <tai2.tran@lge.com>
2024-07-24 11:15:53 +0700

    update

--- src/problems/tmp.cpp
+++ src/problems/tmp.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 #define db(v) cout<<"line("<<__LINE__<<") -> "<<#v<<"="<<v<<endl
+#define int int64_t
 using namespace std;
-
+int n;
 void solve() {
+    
 }
 
-int main() {
+int32_t main() {
     freopen("input.txt","r",stdin);
     solve();
     return 0;
