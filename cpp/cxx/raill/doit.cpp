4a5ab0a4ec0ebd3e62f5b5de3b22b69fa0d588d5
Tai <tai2.tran@lge.com>
2024-08-09 15:12:36 +0700

    update

---
+++ cpp/cxx/raill/doit.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class A {
+public:
+  A() noexcept = default;
+  ~A() { 
+      cout<< "~A() hello" <<endl; 
+    }
+};
+
+
+void f() {
+  A a;
+  //throw "spam";
+}
+
+int main() {
+  /*
+  try { 
+    f(); 
+  } catch (...) 
+  { throw; }
+  */
+  // test kill -9 id
+  f();
+  while(1){}
+}
